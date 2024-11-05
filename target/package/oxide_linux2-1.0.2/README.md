c968d16db512ffd4b9fdd886b058fdc0cadc4352
bensatlantik <bensantora@proton.me>
2024-11-05 09:57:28 -0500

    Update

--- target/package/oxide_linux2-1.0.2/README.md
+++ target/package/oxide_linux2-1.0.2/README.md
@@ -1,5 +1,4 @@
 # oxide_linux2
 
 This Rust library has been deprecated - Nov 5 2024
-
-
+See sysinfo_linux instead
---
+++ target/package/oxide_linux2-1.0.2/target/debug/deps/oxide_linux2-f86c1cdb6eb6e588.d
@@ -0,0 +1,7 @@
+/home/ben/oxide_linux2/target/package/oxide_linux2-1.0.2/target/debug/deps/liboxide_linux2-f86c1cdb6eb6e588.rmeta: src/lib.rs
+
+/home/ben/oxide_linux2/target/package/oxide_linux2-1.0.2/target/debug/deps/liboxide_linux2-f86c1cdb6eb6e588.rlib: src/lib.rs
+
+/home/ben/oxide_linux2/target/package/oxide_linux2-1.0.2/target/debug/deps/oxide_linux2-f86c1cdb6eb6e588.d: src/lib.rs
+
+src/lib.rs:
